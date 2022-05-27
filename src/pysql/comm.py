d38c27ad25b3e5678e1c395dae12cda6cf9a6210
jnsougata <jnsougata@gmail.com>
2022-05-27 07:47:23 +0530

    minor changes

--- src/pysql/comm.py
+++ src/pysql/comm.py
@@ -27,9 +27,9 @@ class PySQL:
     ) -> list:
         if columns:
             cols = ', '.join(columns)
-            query = f'SELECT {cols} FROM {table}'
+            query = f'SELECT DISTINCT {cols} FROM {table}'
         else:
-            query = f'SELECT * FROM {table}'
+            query = f'SELECT DISTINCT * FROM {table}'
 
         if order_by:
             query += f' ORDER BY {", ".join(order_by)}'
@@ -49,13 +49,19 @@ class PySQL:
         for index, val in enumerate(vals):
             if isinstance(val, str):
                 vals[index] = f"'{val}'"
-        modified_values = [str(v) for v in vals if not isinstance(v, str) or v]
+        container = []
+        for val in vals:
+            if not isinstance(val, str) and val is not None:
+                container.append(str(val))
+            elif val is None:
+                container.append('NULL')
+            else:
+                container.append(val)
         query = (f'INSERT INTO {table} ({cols}) '
-                 f'VALUES ({", ".join(modified_values)})')
+                 f'VALUES ({", ".join(container)})')
         self.cursor.execute(query)
         self.connection.commit()
         return {f.name: f.value for f in fields}
-
 
     def create(self, table: str, columns: Dict[str, Types]):
         cols = ', '.join(f'{k} {v.value}' for k, v in columns.items())
--- src/testing/main.py
+++ src/testing/main.py
@@ -1,5 +1,14 @@
 from src.pysql import PySQL, Types, Field, Condition
 
 db = PySQL('root.db')
-data = db.select('users', ['id', 'name', 'age'], condition=Condition.not_equal('name', 'John'))
+db.insert(
+    'users',
+    [
+        Field('id', 100),
+        Field('age', 22),
+        Field('name'),
+
+    ]
+)
+data = db.select('users', ['id', 'name', 'age'], order_by=['age', 'id'])
 print(data)
