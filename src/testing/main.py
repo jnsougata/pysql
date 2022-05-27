955d81b359e6959841958f64ff2a03173bfd537c
jnsougata <jnsougata@gmail.com>
2022-05-27 08:30:17 +0530

    minor changes

--- src/testing/main.py
+++ src/testing/main.py
@@ -1,13 +1,4 @@
 from src.pysql import PySQL, Types, Field, Condition
 
 db = PySQL('root.db')
-db.insert(
-    'users',
-    [
-        Field('id', 100),
-        Field('age', 22),
-        Field('name'),
-
-    ]
-)
-db.show('users')
+db.show('albums')
